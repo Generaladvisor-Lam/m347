b7dad5cbda0385eab9c4153b62afd0cccedd282c
Generaladvisor Lam <113605832+Generaladvisor-Lam@users.noreply.github.com>
2024-03-25 14:29:49 +0100

    upload screenshots =

---
+++ kn02/B/db.php
@@ -0,0 +1,17 @@
+<?php
+      $servername = "kn02b-db";
+      $username = "root";
+      $password = "example";
+      //$dbname = "mysql";
+      // Create connection
+      $conn = new mysqli($servername, $username, $password);
+      // Check connection
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+      $sql = "SELECT Host, User FROM mysql.user;";
+      $result = $conn->query($sql);
+      while ($row = $result->fetch_assoc()) {
+        echo ($row["Host"] . " / " . $row["User"] . "<br />");
+      }
+?>
